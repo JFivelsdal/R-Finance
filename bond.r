0f30e7aa884440653d5e63997df3bb417254222a
Shawn Zhang <always.zhang@gmail.com>
2012-03-06 19:36:01 +0800

    Update bond.r

--- bond.r
+++ bond.r
@@ -4,13 +4,13 @@
 
 
 GetYieldToMaturity <- function (trade_price, time_to_maturity, current_coupon ,eps = 0.00001 ){
-    payment_t <- seq(time_to_maturity %% 0.5, time_to_maturity , by = 0.5)
-    payment_list <- rep(current_coupon, length(payment_t) )
-    payment_list[length(payment_list)] <- payment_list[length(payment_list)] + 100
+  payment_t <- seq(time_to_maturity %% 0.5, time_to_maturity , by = 0.5)
+  payment_list <- rep(current_coupon, length(payment_t) )
+  payment_list[length(payment_list)] <- payment_list[length(payment_list)] + 100
   
-    ror <- 0.05
-    while ( abs(trade_price - sum (payment_list * (1 + ror)^(-payment_t)) ) >= delta ){
-        ror <- ror - ( trade_price - sum(payment_list * (1 + ror)^(-payment_t)) )/(sum(payment_list * payment_t * (1 + ror)^(-payment_t-1)))
-    }
-    return(ror)
+  ror <- 0.05
+  while ( abs(trade_price - sum (payment_list * (1 + ror)^(-payment_t)) ) >= delta ){
+    ror <- ror - ( trade_price - sum(payment_list * (1 + ror)^(-payment_t)) )/(sum(payment_list * payment_t * (1 + ror)^(-payment_t-1)))
+  }
+  return(ror)
 }
