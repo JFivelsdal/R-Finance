01e076dea4fcfae46c15e242a5f4af0820e57614
Shawn Zhang <always.zhang@gmail.com>
2012-03-26 18:51:22 +0800

    add var.R

---
+++ var.R
@@ -0,0 +1,29 @@
+# Parametric Approaches
+
+ParametricVAR <- function(d, a=0.05, method=qnorm){
+  m <- mean(d)
+  std <- sd(d)
+  return (-m + method(a) * std)
+}
+
+LognormalVAR <- function(p, r, a=0.05, method=qnorm){
+  return (p * (1 - exp(mean(r) - sd(r) * method(a))))
+}
+
+# Non-Parametric Approaches
+
+# Age- weighted Historic Approaches
+
+AgeWeight <- function(l, n){
+  rlt <- rep(NA, n)
+  for (i in seq(1,n)){
+    rlt[i] <- l ^ (i - 1) * (1 - l) / (1 - l ^ n)
+  }
+  return(rlt)
+}
+
+# Volatility- Weighted Historical Approaches
+
+VolatilityWeight <- function(T, t, r){
+  return((T / t) * r)
+}
