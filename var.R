45dddcd7b41531555421a32f9e726c54bd76170c
Shawn Zhang <always.zhang@gmail.com>
2012-03-27 22:29:35 +0800

    add POT var , es

--- var.R
+++ var.R
@@ -33,3 +33,12 @@ VolatilityWeight <- function(T, t, r){
 LR <- function(p, n, T){
   return (-2 * log(((1 - p)^(T-n)) * p^n) + 2 * log((1 - n/T)^(T-n) * (n/T)^n ))
 }
+
+POTVAR <- function(u, b, s, n, N, a=0.05){
+  return(u + (b/s) * ((n/N * (1 - a))^(-s) - 1))
+}
+
+POTES <- function(var, b, u, s){
+  return((var + b - s * u)/(1 - s))
+}
+
