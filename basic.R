37bc29fa00a6c713b8c18d9531568777f5ca7057
Shawn Zhang <always.zhang@gmail.com>
2012-05-29 19:08:16 +0800

    "add VasicekModel IR path"

--- basic.R
+++ basic.R
@@ -16,3 +16,17 @@ DiscountFactor <- function(discount_rate, t, continuous = T){
 CAPM <- function(a, b, rf, rm){
   return(a + b(rm - rf))
 }
+
+VasicekModel <- function(a, b, n, delta_t, sigma, r = 0.05, dm = 0, dd = 1){
+  # b: "long term mean level". All future trajectories of  will evolve around a mean level b in the long run;
+  # a: "speed of reversion".  characterizes the velocity at which such trajectories will regroup around  in time;
+  # delta: "instantaneous volatility", measures instant by instant the amplitude of randomness entering the system. Higher  implies more randomness
+  rlt <- r
+  while ( n > 1 ){
+    dr <- a * (b - r) * delta_t + sigma * rnorm(dd, dm)
+    rlt <- append(rlt, r + dr)
+    r <- dr + r
+    n <- n - 1
+  }
+  return(rlt)
+}
